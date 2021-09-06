976ebb95af25d499d183c3ace21a518c3ef9407b
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-09-06 15:15:09 +1000

    feat: data page stats

--- src/graphql.ts
+++ src/graphql.ts
@@ -14,34 +14,31 @@ import graphqlFields from 'graphql-fields'
 import type {GraphQLFieldConfig, GraphQLFieldConfigMap} from 'graphql'
 import type {Response} from 'node-fetch'
 
-interface WithUpdated {
+type WithUpdated<T> = T & {
   readonly updated: string
 }
 
-type HomePageStat =
-  | 'activeCases'
-  | 'deaths'
-  | 'hotelCases'
-  | 'interstateCases'
-  | 'localCases'
-  | 'tests'
+type Stats<T extends string> = Readonly<Record<T, string>>
 
-type HomePageStatsOnly = Readonly<Record<HomePageStat, string>>
+const homePageStats = [
+  'localCases',
+  'interstateCases',
+  'hotelCases',
+  'deaths',
+  'activeCases',
+  'tests'
+] as const
+type HomePageStat = typeof homePageStats[number]
 
-type HomePageStats = HomePageStatsOnly & WithUpdated
-
-const nonNullString: GraphQLFieldConfig<unknown, unknown> = {
-  type: new GraphQLNonNull(GraphQLString)
-}
-
-const updatedField: GraphQLFieldConfigMap<unknown, unknown> = {
-  updated: nonNullString
-}
-
-const withUpdated = new GraphQLInterfaceType({
-  name: 'WithUpdated',
-  fields: updatedField
-})
+const dataPageStats = [
+  'totalCases',
+  'totalDeaths',
+  'recovered',
+  'totalTests',
+  'hospitalCases',
+  'icuCases'
+] as const
+type DataPageStat = typeof dataPageStats[number]
 
 /* eslint-disable @typescript-eslint/ban-types -- object and {} */
 type Fields<T extends object> = {
@@ -55,18 +52,24 @@ type Fields<T extends object> = {
 }
 /* eslint-enable @typescript-eslint/ban-types */
 
-const NAME_TO_IDS: Readonly<Record<HomePageStat, string>> = {
+type AnyStat = DataPageStat | HomePageStat
+
+const NAME_TO_IDS: Readonly<Record<AnyStat, string>> = {
   localCases: 'c429cc59-6887-4093-a937-e7592485f293',
   interstateCases: '2e5c92a1-1c9d-48c9-adf5-a56f096ad99f',
   hotelCases: '05f695de-a635-4c35-a6d1-b6a3d63e02de',
-  deaths: 'd7d13b8d-4a41-435f-8e82-b8d1d5475027',
   activeCases: '9d3a45ca-4e54-4545-9159-d09197bc45d4',
-  tests: '179c4b61-2d74-4472-ac94-9c979a39793d'
-  // hospitalCases: '0c37c1dc-01ad-42c4-88b3-acdf4a1eea88',
-  // icuCases: 'a7bfc1e1-1b7f-4335-8a15-cd585e1cb6df'
+  totalCases: '2612d038-ca63-4cfd-beeb-8ad0a6d83c0e',
+  deaths: 'd7d13b8d-4a41-435f-8e82-b8d1d5475027',
+  totalDeaths: '0e539187-308d-4924-a9df-31df1d1407fe',
+  recovered: '4d573de6-a0b9-4cb6-b45b-9b0e018f7149',
+  tests: '179c4b61-2d74-4472-ac94-9c979a39793d',
+  totalTests: '35208240-6a54-468b-9b6c-b9a0252ce5af',
+  hospitalCases: '0c37c1dc-01ad-42c4-88b3-acdf4a1eea88',
+  icuCases: 'a7bfc1e1-1b7f-4335-8a15-cd585e1cb6df'
 }
 
-const IDS_TO_NAME: Readonly<Record<string, HomePageStat>> = Object.fromEntries(
+const IDS_TO_NAME: Readonly<Record<string, AnyStat>> = Object.fromEntries(
   (
     Object.entries(NAME_TO_IDS) as readonly (readonly [HomePageStat, string])[]
   ).map(([name, id]) => [id, name])
@@ -98,103 +101,114 @@ const fetchJSONAPI = async <T>(url: string, message: string): Promise<T> => {
   return response.data
 }
 
-/* fetchJSONAPI<{
-  readonly attributes: {
-    // eslint-disable-next-line @typescript-eslint/naming-convention -- API response
-    readonly field_paragraph_body: {readonly processed: string}
+const nonNullString: GraphQLFieldConfig<unknown, unknown> = {
+  type: new GraphQLNonNull(GraphQLString)
+}
+
+const updatedField: GraphQLFieldConfigMap<unknown, unknown> = {
+  updated: nonNullString
+}
+
+const withUpdated = new GraphQLInterfaceType({
+  name: 'WithUpdated',
+  fields: updatedField
+})
+
+const statsField = <T extends AnyStat>(
+  name: string,
+  statKeys: readonly T[],
+  fetchUpdated: () => Promise<string>
+): GraphQLFieldConfig<unknown, unknown> => {
+  type OnlyStats = Stats<T>
+  type Result = WithUpdated<OnlyStats>
+  type PartialResult = Partial<Result>
+  return {
+    type: new GraphQLNonNull(
+      new GraphQLObjectType({
+        name,
+        interfaces: [withUpdated],
+        fields: {
+          ...updatedField,
+          ...Object.fromEntries(statKeys.map(s => [s, nonNullString]))
+        }
+      })
+    ),
+    resolve: async (_, __, ___, info): Promise<PartialResult> => {
+      const fields = Object.keys(
+        graphqlFields(info) as Fields<Result>
+      ) as readonly (T | 'updated')[]
+      const [updated, stats] = await Promise.all([
+        fetchUpdated(),
+        fetchJSONAPI<
+          readonly {
+            readonly id: string
+            readonly attributes: {
+              // eslint-disable-next-line @typescript-eslint/naming-convention -- API response
+              readonly field_item_statistic: string
+            }
+          }[]
+        >(
+          `https://content.vic.gov.au/api/v1/paragraph/daily_update_statistics?${qs.stringify(
+            {
+              filter: {
+                c: {
+                  path: 'id',
+                  operator: 'IN',
+                  value: fields
+                    .filter((field): field is T => field !== 'updated')
+                    .map(field => NAME_TO_IDS[field])
+                }
+              }
+            }
+          )}`,
+          'fetching stats failed'
+        ).then(
+          data =>
+            Object.fromEntries(
+              data.map(({id, attributes: {field_item_statistic: stat}}) => [
+                IDS_TO_NAME[id]!,
+                stat
+              ])
+            ) as Partial<OnlyStats>
+        )
+      ])
+      return {updated, ...stats} as PartialResult
+    }
   }
-}>(
-  'https://content.vic.gov.au/api/v1/paragraph/basic_text/7672e694-4dce-4f07-81fb-638b689bb242',
-  'fetching case stats updated text failed'
-).then(({attributes}) =>
-  attributes.field_paragraph_body.processed.slice(
-    '<h2>Updated: '.length,
-    -'</h2>'.length
-  )
-) */
+}
 
 export default new ApolloServer({
   schema: new GraphQLSchema({
     query: new GraphQLObjectType({
       name: 'Query',
       fields: {
-        homePageStats: {
-          type: new GraphQLNonNull(
-            new GraphQLObjectType({
-              name: 'HomePageStats',
-              interfaces: [withUpdated],
-              fields: {
-                ...updatedField,
-                ...Object.fromEntries(
-                  [
-                    'localCases',
-                    'interstateCases',
-                    'hotelCases',
-                    'deaths',
-                    'activeCases',
-                    'tests'
-                  ].map(s => [s, nonNullString])
-                )
+        homePageStats: statsField('HomePageStats', homePageStats, async () =>
+          cheerio
+            .load(
+              await (await fetch('https://www.coronavirus.vic.gov.au')).text()
+            )('.ch-daily-update__intro-title')
+            .text()
+            .slice(
+              ' COVID-19 in Victoria, '.length,
+              -' (last 24 hours )'.length
+            )
+        ),
+        dataPageStats: statsField('DataPageStats', dataPageStats, async () =>
+          (
+            await fetchJSONAPI<{
+              readonly attributes: {
+                // eslint-disable-next-line @typescript-eslint/naming-convention -- API response
+                readonly field_paragraph_body: {readonly processed: string}
               }
-            })
-          ),
-          resolve: async (
-            _,
-            __,
-            ___,
-            info
-          ): Promise<Partial<HomePageStats>> => {
-            const fields = Object.keys(
-              graphqlFields(info) as Fields<HomePageStats>
-            ) as readonly (keyof HomePageStats)[]
-            const [updated, stats] = await Promise.all([
-              fetch('https://www.coronavirus.vic.gov.au').then(async response =>
-                cheerio
-                  .load(await response.text())('.ch-daily-update__intro-title')
-                  .text()
-                  .slice(
-                    ' COVID-19 in Victoria, '.length,
-                    -' (last 24 hours )'.length
-                  )
-              ),
-              fetchJSONAPI<
-                readonly {
-                  readonly id: string
-                  readonly attributes: {
-                    // eslint-disable-next-line @typescript-eslint/naming-convention -- API response
-                    readonly field_item_statistic: string
-                  }
-                }[]
-              >(
-                `https://content.vic.gov.au/api/v1/paragraph/daily_update_statistics?${qs.stringify(
-                  {
-                    filter: {
-                      c: {
-                        path: 'id',
-                        operator: 'IN',
-                        value: fields
-                          .filter(
-                            (field): field is HomePageStat =>
-                              field !== 'updated'
-                          )
-                          .map(field => NAME_TO_IDS[field])
-                      }
-                    }
-                  }
-                )}`,
-                'fetching homepage stats updated text failed'
-              ).then<Partial<HomePageStatsOnly>>(data =>
-                Object.fromEntries(
-                  data.map(({id, attributes: {field_item_statistic: stat}}) => [
-                    IDS_TO_NAME[id]!,
-                    stat
-                  ])
-                )
-              )
-            ])
-            return {updated, ...stats}
-          }
-        },
+            }>(
+              'https://content.vic.gov.au/api/v1/paragraph/basic_text/7672e694-4dce-4f07-81fb-638b689bb242',
+              'fetching case stats updated text failed'
+            )
+          ).attributes.field_paragraph_body.processed.slice(
+            '<h2>Updated: '.length,
+            -'</h2>'.length
+          )
+        ),
         exposureSites: {
           type: new GraphQLNonNull(
             new GraphQLObjectType({
