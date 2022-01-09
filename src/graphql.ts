921be2652f411a381187e24464e339b26bf1315c
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2022-01-09 15:56:19 +1100

    feat: update for stats website changes and rename things

--- src/graphql.ts
+++ src/graphql.ts
@@ -43,27 +43,27 @@ type WithUpdated<T> = T & {
 type Stats<T extends string> = Readonly<Record<T, string>>
 
 const homePageStats = [
-  'localCases',
-  'activeCases',
+  'dose1',
+  'dose2',
+  'dose3',
+  'newCases',
+  'pcrTests',
+  'ratTests',
   'hospitalCases',
   'icuCases',
-  'ventilatorCases',
-  'totalHospitalCases',
-  'firstDose12',
-  'secondDose12',
-  'firstDose16',
-  'secondDose16'
+  'deaths'
 ] as const
 type HomePageStat = typeof homePageStats[number]
 
 const dataPageStats = [
-  'hotelCases',
-  'totalCases',
-  'deaths',
+  'newPCRCases',
+  'newRATCases',
+  'newHotelCases',
+  'activeCases',
+  'totalPCRTests',
+  'totalPCRCases',
   'totalDeaths',
-  'recovered',
-  'tests',
-  'totalTests'
+  'recovered'
 ] as const
 type DataPageStat = typeof dataPageStats[number]
 
@@ -82,23 +82,23 @@ const COVID_SITE = 'https://www.coronavirus.vic.gov.au'
 const ABC_SITE = 'https://www.abc.net.au'
 
 const NAME_TO_IDS: Readonly<Record<AnyStat, string>> = {
-  localCases: 'c429cc59-6887-4093-a937-e7592485f293',
-  hotelCases: '0b3768b7-b8b6-43d5-842d-fb6a07e7a61b',
-  activeCases: '05f695de-a635-4c35-a6d1-b6a3d63e02de',
-  totalCases: '2612d038-ca63-4cfd-beeb-8ad0a6d83c0e',
-  deaths: 'e0900375-0198-47a2-9d88-70909977e395',
+  dose1: 'bd3dad0d-5c68-4fc6-a392-e7f22f1e734d',
+  dose2: 'a95c18ed-7111-4e54-9936-5ec4fe135058',
+  dose3: '74b2a8a1-4edb-4cb2-96d5-d1bf96ec3b21',
+  newCases: 'c429cc59-6887-4093-a937-e7592485f293',
+  pcrTests: '05f695de-a635-4c35-a6d1-b6a3d63e02de',
+  ratTests: 'd7d13b8d-4a41-435f-8e82-b8d1d5475027',
+  hospitalCases: '9d3a45ca-4e54-4545-9159-d09197bc45d4',
+  icuCases: '2e5c92a1-1c9d-48c9-adf5-a56f096ad99f',
+  deaths: '179c4b61-2d74-4472-ac94-9c979a39793d',
+  newPCRCases: '293615f7-f87f-4bc0-954c-1bb53989e6fc',
+  newRATCases: '0b3768b7-b8b6-43d5-842d-fb6a07e7a61b',
+  newHotelCases: '5c5d8d1b-89e3-4a5e-9fcf-0b93da140e9d',
+  activeCases: 'a0681e4b-82d0-4188-a6d3-b3f2789dd110',
+  totalPCRTests: '35208240-6a54-468b-9b6c-b9a0252ce5af',
+  totalPCRCases: '2612d038-ca63-4cfd-beeb-8ad0a6d83c0e',
   totalDeaths: '0e539187-308d-4924-a9df-31df1d1407fe',
-  recovered: '4d573de6-a0b9-4cb6-b45b-9b0e018f7149',
-  tests: '439e0498-93fe-43f0-8069-c89a5529638e',
-  totalTests: '35208240-6a54-468b-9b6c-b9a0252ce5af',
-  hospitalCases: 'd7d13b8d-4a41-435f-8e82-b8d1d5475027',
-  icuCases: '9d3a45ca-4e54-4545-9159-d09197bc45d4',
-  ventilatorCases: '2e5c92a1-1c9d-48c9-adf5-a56f096ad99f',
-  totalHospitalCases: '179c4b61-2d74-4472-ac94-9c979a39793d',
-  firstDose12: 'bd3dad0d-5c68-4fc6-a392-e7f22f1e734d',
-  secondDose12: 'a95c18ed-7111-4e54-9936-5ec4fe135058',
-  firstDose16: '74b2a8a1-4edb-4cb2-96d5-d1bf96ec3b21',
-  secondDose16: '0537d785-88dc-4810-b664-75b60e480783'
+  recovered: '4d573de6-a0b9-4cb6-b45b-9b0e018f7149'
 }
 
 const IDS_TO_NAME: Readonly<Record<string, AnyStat>> = Object.fromEntries(
@@ -149,13 +149,13 @@ const covidAPI = async <T>(
   return response.data
 }
 
-const fetchUpdated = async (path: string, message: string): Promise<string> =>
+const fetchUpdated = async (id: string, message: string): Promise<string> =>
   (
     await covidAPI<{
       readonly attributes: {
         readonly changed: string
       }
-    }>(path, `${message} updated`, {
+    }>(`block_content/daily_update/${id}`, `${message} updated`, {
       fields: {'block_content--daily_update': 'changed'}
     })
   ).attributes.changed
@@ -267,34 +267,34 @@ export default new ApolloServer({
               homePageUpdated,
               dataPageUpdated,
               {
-                localCases,
-                hotelCases,
-                activeCases,
-                totalCases,
-                deaths,
-                totalDeaths,
-                recovered,
-                tests,
-                totalTests,
+                dose1,
+                dose2,
+                dose3,
+                newCases,
+                pcrTests,
+                ratTests,
                 hospitalCases,
                 icuCases,
-                ventilatorCases,
-                totalHospitalCases,
-                firstDose12,
-                secondDose12,
-                firstDose16,
-                secondDose16
+                deaths,
+                newPCRCases,
+                newRATCases,
+                newHotelCases,
+                activeCases,
+                totalPCRTests,
+                totalPCRCases,
+                totalDeaths,
+                recovered
               }
             ] = await Promise.all([
               fields.homePage?.updated
                 ? fetchUpdated(
-                    'block_content/daily_update/743c618f-deb7-4f00-9eb3-c4abc1171663',
+                    '743c618f-deb7-4f00-9eb3-c4abc1171663',
                     'home page'
                   )
                 : undefined,
               fields.dataPage?.updated
                 ? fetchUpdated(
-                    'block_content/daily_update/e674178a-0717-44c1-a14f-514db0e1dc65',
+                    'e674178a-0717-44c1-a14f-514db0e1dc65',
                     'data page'
                   )
                 : undefined,
@@ -339,26 +339,26 @@ export default new ApolloServer({
             return {
               homePage: {
                 updated: homePageUpdated,
-                localCases,
-                activeCases,
+                dose1,
+                dose2,
+                dose3,
+                newCases,
+                pcrTests,
+                ratTests,
                 hospitalCases,
                 icuCases,
-                ventilatorCases,
-                totalHospitalCases,
-                firstDose12,
-                secondDose12,
-                firstDose16,
-                secondDose16
+                deaths
               },
               dataPage: {
                 updated: dataPageUpdated,
-                hotelCases,
-                totalCases,
-                deaths,
+                newPCRCases,
+                newRATCases,
+                newHotelCases,
+                activeCases,
+                totalPCRTests,
+                totalPCRCases,
                 totalDeaths,
-                recovered,
-                tests,
-                totalTests
+                recovered
               }
             }
           }
